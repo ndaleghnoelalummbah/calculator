c1b733ac7c52be3597d43fc94bd457f499e92095
ndaleghnoelalummbah <ndaleghnoela@gmail.com>
2022-03-18 13:38:34 +0100

    add interactive functionalities

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include<stdio.h>
-int main(void)
+float add();
+float subtract();
+float divide();
+float multiply();
+int main(void)	
 {
-	char choice[20];
-	printf("enter choice of operation:");
-	scanf("s",&choice);
+	char choice;
+	float x,y;
+	while(1)
+	{
+		printf("enter choice of operation:");
+		scanf("%c",&choice);
+		fflush(stdin);	
+		if(choice=='+')
+		add(x,y);
+		if(choice=='-')
+		subtract(x,y);
+		if(choice=='*')
+		multiply(x,y);
+		if(choice=='/')
+		divide(x,y);
+	}
+
 	return 0;
 }
+float add()
+	{
+		//
+	}
+
+float subtract()
+	{
+		//
+	}
+float multiply()
+	{
+		//
+	}
+float divide()
+	{
+		//
+	}
+
+
+
